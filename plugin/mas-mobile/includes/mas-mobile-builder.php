ac2713b202879fd44607b6d838d155c455584eb7
Thuy Luu <thuyluu@magenest.com>
2022-09-06 15:25:33 +0700

    fixbug

--- plugin/mas-mobile/includes/mas-mobile-builder.php
+++ plugin/mas-mobile/includes/mas-mobile-builder.php
@@ -264,8 +264,8 @@ final class MasMobileBuilder
         }
         $encodedShopName = get_post_meta($postId,'shop_name',true);
         //masmb
-        $shopName = $encodedShopName.substr(0,$encodedShopName.length - 5);
-        $output['base_url'] ='https://'.$shopName.'myshopify.com';
+        $shopName = substr($encodedShopName,0,$encodedShopName.length - 5);
+        $output['base_url'] ='https://'.$shopName.'.myshopify.com';
         $output['currencyCode'] ='USD';
         $output['app_state'] =true;
 
