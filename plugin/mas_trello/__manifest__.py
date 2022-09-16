708918194b58c62659a6fbbf7745021750ae681a
Thuy Luu <thuyluu@magenest.com>
2022-09-16 14:41:20 +0700

    commit

--- plugin/mas_trello/__manifest__.py
+++ plugin/mas_trello/__manifest__.py
@@ -24,10 +24,10 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
         'data/app_data.xml',
         'data/group_plan.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
